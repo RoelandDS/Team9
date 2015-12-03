a023ab0086d1990e1865ebabba995a70c3fcd049
tghmember <rclaes1995@gmail.com>
2015-12-03 12:36:59 +0000

    allow-origin fix

--- htdocs/server.js
+++ htdocs/server.js
@@ -2,8 +2,28 @@ var data = { "name": "Team 9", "members": [{"name": "Glenn"},{"name": "Robin"}] 
 var http = require('http');
 var request = require('request');
 var express = require('express');
+var port = process.env.PORT || 4000;
 
 var app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // OPDRACHT 2
 // request({
@@ -25,4 +45,4 @@ app.get('/', function(req, res)
     res.send(data);
 });
 
-app.listen(process.env.PORT,process.env.IP);
+app.listen(port,process.env.IP);
